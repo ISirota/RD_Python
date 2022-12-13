6ad8a7f80969518b02390cf31ee4516cfd030ccf
Ihor0789 <4ks@ukr.net>
2022-12-13 08:28:03 +0200

    add home work 3  -  file dz3.py

---
+++ dz3.py
@@ -0,0 +1,23 @@
+
+print(42 * 'I love Python')
+
+age_in_month = 530
+age_in_years = age_in_month // 12
+my_name = 'Ihor'
+my_age = 'Му name is ' + my_name + '. I’m ' + str(age_in_years) + ' years old.'
+
+print(my_age)
+
+number_one = 1
+
+print (' одиниця більше нуля?' ,number_one > 0)
+print (' одиниця рівна нулю?' ,number_one == 0)
+print (' одиниця менше нуля?' ,number_one < 0)
+print (' одиниця більше або рівно нулю?' ,number_one >= 0)
+print (' одиниця нерівна нулю?' ,number_one != 0)
+
+a,b,c = 2,5,6
+list_sum = str(a)+ str(b) + str(c)
+print (list_sum, 'tupe -', type(list_sum))
+sum = int(list_sum)
+print (sum, 'tupe -', type(sum))
