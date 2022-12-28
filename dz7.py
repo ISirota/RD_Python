f8c0cd724fab0fedb6766980329cef7e3ecbd31f
Ihor <4KS@UKR.NET>
2022-12-28 18:44:06 +0200

    Update dz7.py
    Виконано виправленя  помилки.  Приведенно до рекомендації за стандартом PEP 8.

--- dz7.py
+++ dz7.py
@@ -11,30 +11,31 @@ show <name>: детальна інформація по імені
 telefon_book = {}
 
 while True:
-    command_input  = input (' Введіть команду (stats, add, delete, list, show):  ')
+    command_input = input(' Введіть команду (stats, add, delete, list, show):  ')
     if command_input == 'stats':
-        print ('Кількість записів' , len(telefon_books))
+        print('Кількість записів', len(telefon_book))
     elif command_input == 'add':
         new_name = input("введіть ім'я нового запису: ")
-        if telefon_book.get(new_name) != None:
-            print(' Запису під іменем ', new_name , 'у телефонній книзі існує. Ви не можете змінити номер. Рекомендую видалити запис та додати новий.')
+        if telefon_book.get(new_name) is not None:
+            print(' Запису під іменем ', new_name, 'у телефонній книзі існує. ')
+            print(' Ви не можете змінити номер. Рекомендую видалити запис та додати новий.')
         else:
             new_fone = input("введіть номер телефону: ")
             telefon_book[new_name] = new_fone
     elif command_input == 'delete':
         old_name = input("введіть ім'я  запису, що видаляємо : ")
-        if telefon_book.get(old_name) == None:
-            print(' Запису під іменем ', old_name , 'у телефонній книзі не існує.')
+        if telefon_book.get(old_name) is None:
+            print(' Запису під іменем ', old_name, 'у телефонній книзі не існує.')
         else:
             telefon_book.pop(old_name)
-            print ('видалено запис для ', old_name)
+            print('видалено запис для ', old_name)
     elif command_input == 'list':
-        print (*list(telefon_book.keys()),sep="\n")
+        print(*list(telefon_book.keys()), sep="\n")
     elif command_input == 'show':
         show_name = input("введіть ім'я  запису, який Вас цікавить : ")
-        if telefon_book.get(show_name) == None:
+        if telefon_book.get(show_name) is None:
             print(' Запису під іменем ', show_name , 'у телефонній книзі не існує.')
         else:
-            print ('номер телефону для ' ,show_name, ' - ',telefon_book.get(show_name))
+            print('номер телефону для ', show_name, ' - ', telefon_book.get(show_name))
     else:
-        print ('  Я не знаю такої команди.')
+        print('  Я не знаю такої команди.')
