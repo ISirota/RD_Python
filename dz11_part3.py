ffa25c6c83989fdc49c64ae85d565852897f2be5
Ihor <4KS@UKR.NET>
2023-01-21 22:14:13 +0200

    внесено зміни у файл dz11_part3.py. гарний та  короткий запис. не знав. дякую.

--- dz11_part3.py
+++ dz11_part3.py
@@ -10,35 +10,8 @@ try:
     print(10 * '=')
     k = 5 / 4
     #k = 5+'gdf'
-    #print(my_list['error'])
+    #print(my_list[gf])
 
-
-except AttributeError:
-    print(' Custom exception is occured - AttributeError')
-except ArithmeticError:
-    print(' Custom exception is occured - ArithmeticError')
-except EOFError:
-    print(' Custom exception is occured - EOFError')
-except NameError:
-    print(' Custom exception is occured - NameError')
-except LookupError:
-    print(' Custom exception is occured - LookupError')
-except StopIteration:
-    print(' Custom exception is occured - StopIteration')
-except OSError:
-    print(' Custom exception is occured - OSError')
-except TypeError:
-    print(' Custom exception is occured - TypeError')
-except ValueError:
-    print(' Custom exception is occured - ValueError')
-
-# можна і більш глобально -але тоді не вкажемо тип помилки
-#except Exception:
-#    print ('Error')
-
-else:
-    print('No error')
-
-finally:
-    print('The end')
-    print(10 * '=')
+except Exception as e:
+    print(f'{e}')
+print(10 * '=')
