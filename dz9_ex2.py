2435647e96f19e99d42c9792ab33cd4f9c3f18f5
Ihor <4KS@UKR.NET>
2023-01-27 23:08:22 +0200

    add file dz9_ex2.py

--- dz9_ex2.py
+++ dz9_ex2.py
@@ -1,16 +1,21 @@
 # інтератор числа Фібоначчі
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        a, b = 0, 1
-        for nom in range(2, n+1):
-            a, b = b, a + b
-        return b
+class Fibo:
+    def __init__(self, max_count):
+        self.max_count = max_count
+        self.count = 0
+        self.first, self.second = 1, 1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.count += 1
+        if self.count > self.max_count:
+            raise StopIteration
+        elif self.count > 2:
+            self.first, self.second = self.second, self.first + self.second
+            return self.second
+        else:
+            return 1
 
 n = int(input("Ведіть порядковий номер елемента послідовності Фібоначчі "))
-
-print("Елемент послідовності Фібоначчі за порядковим номером ", n, ' = ', fib(n))
-
+for f in Fibo(n):
+    print(f)
