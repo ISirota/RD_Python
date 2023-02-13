cc56cae2b985f542c3d3e4858cd5fa3701a3d083
Ihor <4KS@UKR.NET>
2023-02-13 23:37:39 +0200

    add sql file for hw20

--- dz20_1.sql
+++ dz20_1.sql
@@ -1,4 +1,3 @@
-
 create table main.users
 (
     id         integer /*autoincrement needs PK*/,
---
+++ dz20_2.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO users(first_name, last_name, age) VALUES ('Btestkh','Guem',232);
+INSERT INTO users(first_name, last_name, age) VALUES ('name1','name2',5);
+INSERT INTO users(first_name, last_name, age) VALUES ('Leonid','Kuchma',42);
+INSERT INTO users(first_name, last_name, age) VALUES ('Dan','Balan',27);
+INSERT INTO users(first_name, last_name, age) VALUES ('Top','Gum',21);
+INSERT INTO users(first_name, last_name, age) VALUES ('Viktoria','Lisova',33)
+
