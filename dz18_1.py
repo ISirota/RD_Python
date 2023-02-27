7d2f624002696f595126d87cebb6f04c83d3dcaf
Ihor <4KS@UKR.NET>
2023-02-27 23:58:08 +0200

    add dz 18

--- dz18_1.py
+++ dz18_1.py
@@ -2,3 +2,20 @@
 #
 # send_message має приймати параметри self і message і має друкувати message.
 ## Метод say_name має друкувати значення атрибуту name.
+
+class Bot:
+    name = "name"
+    def say_name(name):
+        print(name)
+    def send_message (self, message):
+        print(message)
+
+
+
+
+
+
+print ('атрибут -', Bot.name)
+Bot.say_name('Robinzon')
+Bot.send_message('ggg', 'bbb')
+
